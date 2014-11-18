6f3a939a5e2cc65b2fdec501b3c0aa9550c589be
Rob Widmer <rdwidmer@gmail.com>
2014-11-17 21:32:19 -0500

    Official release!

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-whileinuse
+While In Use
 ==========
+This small utility (less than 400 bytes minified) allows you to schedule a callback to run on a given interval, but only if activity is detected on the page since the last time the callback ran. The callback will always execute after the first interval, regardless of whether any activity has happened on the page.
+
+## Usage
+
+To schedule a callback, pass it and an interval (in milliseconds) to $.whenInUse.
+    
+    function makeAwesomeSauce() {
+      console.log('Sweet!');
+    }
+    
+    $.whenInUse(makeAwesomeSauce, 1000);
+
+To discontinue an interval, explicitly return false from the callback.
+    
+    //Only run this callback 4 times
+    var count = 0;
+    
+    function stopAtFour() {
+        return ++count != 4;
+    }
+    
+    $.whenInUse(stopAtFour, 5000);
+
+## Requirements
+Requires [jQuery](http://jquery.com) - Tested with 2.1.1 but should work with any version that supports .one()
+
+## License
+
+Copyright (c) 2014 Rob Widmer.
+While In Use is released under the MIT License. See the bundled LICENSE file for
+details.
---
+++ jquery.whileinuse.js
@@ -0,0 +1,54 @@
+/*!
+ * When In Use jQuery plugin v0.0.1
+ * http://jquery.com/
+ *
+ * Copyright 2014 Rob Widmer
+ * Released under the MIT license
+ * http://github.com/rdubya/whileinuse
+ */
+(function($, window){
+	'use strict';
+
+	var internalSetTimeout = window.setTimeout, // Save a few bytes when minified
+		$window = $(window); // Avoid wrapping the window object every time
+
+	function listenOnce(func) {
+		$window.one('mousemove touchstart', func);
+	}
+
+	$.whileInUse = function(action, interval) {
+		var active = true; // Default this to true so it will always run the first time
+
+		// Only create this function once so it is reused when the handler to register activity fires
+		function markActive() {
+			active = true;
+		}
+
+		// Only create this function once so it is reused when the handler for switching from inactive to active fires
+		function executeAsync() {
+			// asynchronously execute with a slight delay so a small mouse movement
+			// only runs the action once and doesn't count as still being active
+			// for the next interval
+			internalSetTimeout(runner, 500);
+		}
+
+		function runner() {
+			if (active) {
+				active = false;
+
+				// If the action returns false we will stop the interval
+				// returning nothing or true will continue the interval
+				if (action() != false) {
+					internalSetTimeout(runner, interval);
+					listenOnce(markActive);
+				}
+			} else {
+				// No events have fired since the last execution
+				// Set up a handler to restart the intervals when action happens
+				listenOnce(executeAsync);
+			}
+		}
+
+		internalSetTimeout(runner, interval);
+	};
+})(jQuery, window);
